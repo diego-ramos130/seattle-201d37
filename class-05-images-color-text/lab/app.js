10364f1c228cef786206fd192a166acf8a883cdb
diego-ramos130 <diego.onethreezero@gmail.com>
2018-07-13 15:31:18 -0700

    GET FUCKED TESTARRAY

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -66,6 +66,7 @@ var total = sum(t1,c)[0];
 var t2 = multiply(a,b)[0];
 var product = multiply(t2,c)[0]; */
 //if this somehow works, i will fucking nut in my pants.
+//update: i have nutted.
 var total = sum(sum(a,b)[0],c)[0];
 var product = multiply(multiply(a,b)[0],c)[0];
 var arr = []; 
@@ -79,7 +80,7 @@ return arr;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
- testSumAndMultiply(4,7,5);
+// testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -97,12 +98,20 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-  
+var sumtotal = 0;
+for (var i = 0; i < sumArr.length; i++){
+  sumtotal = sum(sumtotal, (sumArr[i] ))[0];
+}
+var arr = [];
+arr.push(sumtotal);
+var sumMsg = sumArr.join() + ' was passed in as an array of numbers, and ' + sumtotal + ' is their sum.';
+arr.push(sumMsg);
+return arr;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
